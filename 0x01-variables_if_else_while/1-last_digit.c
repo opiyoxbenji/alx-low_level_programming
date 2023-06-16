79322285599a0f61ebde68bd929466a67107f556
opiyoxbenji <opiyosbenji@gmail.com>
2023-06-16 13:14:09 +0300

    a program that prints the alphabet in lowercase, followed by a new line and excludes q and e

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,24 +8,21 @@
  */
 int main(void)
 {
-	int n;
+	int n, m;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	printf("Last digit of %d\n", n);
+	m = n % 10;
 
 	last_digit = n % 10;
 	if (last_digit > 5)
 	{
-		printf("and is greater than 5");
+		printf("Last digit of %d is %d is greater than %5\n", n, m);
 	}
-	else if (last_digit == 0)
-	{
-		printf("and is 0");
+	else if (last_digit == 0){
 	}
 	else if (last_digit < 6 && last_digit != 0)
 	{
-		printf("and is less than 6 and not 0");
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	}
 }
