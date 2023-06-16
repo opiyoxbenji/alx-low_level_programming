f762229481beeda9fae444adf510b9a4092f1565
opiyoxbenji <opiyosbenji@gmail.com>
2023-06-16 18:19:46 +0300

    a C program that prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,29 +7,19 @@
  */
 int main(void)
 {
-	int combs[] = {01, 02, 03, 04, 05, 06, 07, 08, 09, 12, 13, 14, 15, 16, 17, 18, 19, 23, 24, 25, 26, 27, 28, 29, 34, 35, 36, 37, 38, 39, 45, 46, 47, 48, 49, 56, 57, 58, 59, 67, 68, 69, 78, 79, 89}
-	int num_combs = sizeof(combinations) / sizeof(combinations[0]);
+	int i, j;
 
-	int i;
-
-	for (i = 0; i < num_combs; i++)
+	for (i = 0; i <= 8; i++)
 	{
-		int comb = combs[i];
-		int first_digit = comb / 10;
-		int second_digit = comb % 10;
-
-		putchar('0' + first_digit);
-		putchar('0' + second_digit);
-
-		if (i < num_combs - 1)
+		for (j = i + 1; j <= 9; j++)
 		{
-			putchar('\n');
-			putchar(' ');
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
-
-	putchar('\n');
-
-
-	return (0);
 }
