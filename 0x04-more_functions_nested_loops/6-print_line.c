5527074441efb3500fc9d05a0d8d70b945f54c4f
opiyoxbenji <opiyosbenji@gmail.com>
2023-06-22 12:04:21 +0300

    fix

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number to be checked
+ * Return: 0
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	int i;
+       for (i= 0; i < n; i++)	
+}
