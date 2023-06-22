52626c1a324647312cfa94d6f74ee5af31141715
opiyoxbenji <opiyosbenji@gmail.com>
2023-06-22 15:35:02 +0300

    fizzbuzz challenge

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,14 +11,23 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
+		{
 			printf("FizzBuzz");
+		}
 		else if (i % 3 == 0)
+		{
 			printf("Fizz");
+		}
 		else if (i % 5 == 0)
+		{
 			printf("Buzz");
+		}
 		else
+		{
 			printf("%d", i);
-		printf(" ");
+		}
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 
