929945712cfbbdb1dac010932b8bd93dd63339db
opiyoxbenji <opiyosbenji@gmail.com>
2023-06-22 12:21:03 +0300

    a function that prints 10 times the numbers, from 0 to 14, followed by a new line

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -8,14 +8,8 @@
  */
 int _isdigit(int c)
 {
-	int i = 0;
-
-	while (i < 10)
-	{
-		if (c == i)
-			return (1);
-		i++;
-	}
-
-	return (0);
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
 }
