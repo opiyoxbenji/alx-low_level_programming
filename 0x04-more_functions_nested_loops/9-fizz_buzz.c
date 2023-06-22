fdb13e101a291f649cd8e558d9df092e0151e2b1
opiyoxbenji <opiyosbenji@gmail.com>
2023-06-22 13:35:40 +0300

    {

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,14 +2,13 @@
 
 /**
  * main - prints numbers from 1 to 100
- * 
  * Return: 0
  */
 int main(void)
 {
 	int i;
 
-	for (i = 1; i <=100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
