212ab6684e331bf4b9836cd2ad0d477added6b16
opiyoxbenji <opiyosbenji@gmail.com>
2023-06-22 17:10:48 +0300

    a program that finds and prints the largest prime factor of the number 612852475143

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: finds and prints the largest prime factor
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long num = 612852475143;
+	unsigned long divider = 2;
+
+	while (divider < num)
+	{
+		if (num % divider == 0)
+			num /= divider;
+		else
+			divider++;
+		printf("%lu", num);
+	}
+
+	return (0);
+}
