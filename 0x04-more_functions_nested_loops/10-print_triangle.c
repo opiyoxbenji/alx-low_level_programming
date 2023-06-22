de03fc331be586eeb8676b2f43e5c0531b7ee38d
opiyoxbenji <opiyosbenji@gmail.com>
2023-06-22 15:46:42 +0300

    prints a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,6 +17,10 @@ void print_triangle(int size)
 
 	for (i = 0; i < size; i++)
 	{
+		for (j = 0; j <= size - i - 1; j++)
+		{
+			_putchar(' ');
+		}
 		for (j = 0; j <= i; j++)
 		{
 			_putchar('#');
