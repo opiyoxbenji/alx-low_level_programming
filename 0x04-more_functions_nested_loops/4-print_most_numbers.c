3653e47a9373ad23c4a941a6871962c664c4a9b2
opiyoxbenji <opiyosbenji@gmail.com>
2023-06-22 11:11:11 +0300

    a function that prints the numbers, from 0 to 9, followed by a new line

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,5 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - prints the numbers from 
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #ifndef main_h
 #define main_h
+#include <unistd.h>
 
+int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
