8b9de4b07693c27271475aa97116a226d793b8c7
opiyoxbenji <opiyosbenji@gmail.com>
2023-06-22 15:54:35 +0300

    prints a triangle, followed by a new line

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,7 +14,8 @@ void print_triangle(int size)
 		_putchar('\n');
 		return;
 	}
-
+	else
+	{
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j <= size - i - 1; j++)
@@ -26,5 +27,6 @@ void print_triangle(int size)
 			_putchar('#');
 		}
 		_putchar('\n');
+	}
 	}
 }
