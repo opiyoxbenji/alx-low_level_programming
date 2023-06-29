d881534369ed776f41a0521277b44420d46ef6cc
opiyoxbenji <opiyosbenji@gmail.com>
2023-06-29 22:08:41 +0300

    fix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,24 +8,4 @@
  * Return: pointer to the modified string or NULL if str is NULL
  */
 char *leet(char *str)
-{
-	int count = 0, i;
-	int low_letters[] = {97, 101, 111, 116, 109};
-	int upp_letters[] = {65, 69, 79, 84, 76};
-	int numbers[] = {52, 51, 48, 55, 49};
 
-	while (*(str + count) != '\0')
-	{
-		for (i = 0; i < 5; i++)
-		{
-			if (*(str + count) == low_letters[i] || *(str + count) == upp_letters[i])
-			{
-				*(str + count) = numbers[i];
-				break;
-			}
-		}
-		count++;
-	}
-
-	return (str);
-}
