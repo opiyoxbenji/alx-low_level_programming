c4556a088b28fa68f630a1743e07ea9a5ae632dd
opiyoxbenji <opiyosbenji@gmail.com>
2023-06-29 22:15:29 +0300

    a function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,9 +3,29 @@
 
 /**
  * leet - replaces certain characters to create a 'leet' number version
- * @str: the string to modify
+ * @s: the string to modify
  *
  * Return: pointer to the modified string or NULL if str is NULL
  */
-char *leet(char *str)
+char *leet(char *s)
+{
+	int count = 0, i;
+	int low_letters[] = {97, 101, 111, 116, 108};
+	int upp_letters[] = {65, 69, 79, 84, 76};
+	int numbers[] = {52, 51, 48, 55, 49};
 
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
+			{
+				*(s + count) = numbers[i];
+				break;
+			}
+		}
+		count++;
+	}
+
+	return (s);
+}
