d1bf8db53a2631a49f088b3e5fc852aa7cd40609
opiyoxbenji <opiyosbenji@gmail.com>
2023-07-18 02:20:21 +0300

    0.puppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,21 +7,11 @@
  * @age: dog age
  * @owner: dog owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- * dog_t: new name for struct dog
- */
-typedef struct dog dog_t;
-
-dog_t *new_dog(char *name, float age, char *owner);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-void free_dog(dog_t *d);
+} dog_t;
 
 #endif
