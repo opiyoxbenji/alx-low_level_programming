8c33409dd5581a8011c5d1b45011b2737c4eb0bd
opiyoxbenji <opiyosbenji@gmail.com>
2023-07-18 02:58:52 +0300

    function that creates a new dog.

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,13 +5,20 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		/*print the name or (nil) if it's NULL*/
-		printf("Name: %s\n", d->name ? d->name : "(nil)");
-
-		/*print the age*/
-		printf("Age: %.1f\n", d->age);
-
-		/*print the owner or (nil) if it's NULL*/
-		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+		return;
 	}
+	if (d->name == NULL)
+	{
+		d->name = "(nil)";
+	}
+	if (d->owner == NULL)
+	{
+		d->owner = "(nil)";
+	}
+	/*print the name or (nil) if it's NULL*/
+	printf("Name: %s\n", d->name);
+	/*print the age*/
+	printf("Age: %f\n", d->age);
+	/*print the owner or (nil) if it's NULL*/
+	printf("Owner: %s\n", d->owner);
 }
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,80 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - calculates the length of a string
+ * @s: input string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+/**
+ * _strcpy - copies a string from source to destination
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to the destination string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int  i = 0;
+
+	for (; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+
+/**
+ * new_dog - creates a new dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: dog (Success), NULL otherwise
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int l1 = _strlen(name);
+	int l2 = _strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		return (NULL);
+	}
+
+	dog->name = malloc(sizeof(char) * (l1 + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	dog->owner = malloc(sizeof(char) * (l2 + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
+}
