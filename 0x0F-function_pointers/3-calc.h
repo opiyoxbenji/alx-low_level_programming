3abdef35eb73126b03eb9224bcd21f87d685c496
opiyoxbenji <opiyosbenji@gmail.com>
2023-07-20 04:23:49 +0300

    trial 2

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,27 +1,21 @@
-#ifndef calc_h
-#define calc_h
-
-/* operator fucntions */
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-
-/* function that selects the correct operation */
-
-iny (*get_op_func(char *s)(int, int);
-
+#ifndef _HEADER_
+#define _HEADER_
 /**
- * struct op - struct op
+ * struct op - Struct op
  *
  * @op: The operator
- * @f: The finished associated
+ * @f: The function associated
  */
-typdef struct op
+typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
 
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 #endif
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,34 +1,32 @@
 #include "3-calc.h"
-#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * get_op_func - get ops function pointer of type char array
- *               that accepts two inputs of int data type
- *
- * @s: a character pointer pointing to a symbol from the program argument
- *
- * Return: one of the operator functions to perform calculations
-*/
-
+  * get_op_func - ...
+  * @s: ...
+  *
+  * Return: ...
+  */
 int (*get_op_func(char *s))(int, int)
 {
-	/* struct opts of struct op_t */
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+	{ "/", op_div },
+	{ "%", op_mod },
+	{ NULL, NULL }
 	};
 	int i = 0;
 
 	while (i < 5)
 	{
-		if (*s == *ops[i].op)
+		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
+
 		i++;
 	}
 
-	return (NULL);
+	return (0);
 }
