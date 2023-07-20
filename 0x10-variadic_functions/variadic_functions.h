58330390c95d928390af16e7eafa560191a724d9
opiyoxbenji <opiyosbenji@gmail.com>
2023-07-20 13:45:38 +0300

    function that prints numbers, followed by a new line

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,8 @@
 #ifndef variadic_h
 #define variadic_h
 
+#include <stdarg.h>
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
