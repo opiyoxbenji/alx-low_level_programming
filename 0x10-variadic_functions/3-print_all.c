9955cf25ec79bc5ef1635ffc44954fbba13a1855
opiyoxbenji <opiyosbenji@gmail.com>
2023-07-20 21:05:57 +0300

    function that prints anything.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,6 @@
 #include "variadic_functions.h"
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
  * print_all - function that prints anything
@@ -9,40 +10,37 @@ void print_all(const char * const format, ...)
 {
 	int i = 0;
 	char *string, *separ = "";
-
 	va_list ar;
 
 	va_start(ar, format);
 
-	if (format)
+	while (format[i])
 	{
-		while (format[i])
+		switch (format[i])
 		{
-			switch (format[i])
-			{
-				case 'c':
-					printf("%s%c", separ, va_arg(ar, int));
-					break;
-				case 'i':
-					printf("%s%d", separ, va_arg(ar, int));
-					break;
-				case 'f':
-					printf("%s%f", separ, va_arg(ar, double));
-					break;
-				case 's':
-					string = va_arg(ar, char *);
-					if (!string)
-						string = "(nil)";
-					printf("%s%s", separ, string);
-					break;
-				default:
-					i++;
-					continue;
-			}
+			case 'c':
+				printf("%s%c", separ, va_arg(ar, int));
+				break;
+			case 'i':
+				printf("%s%d", separ, va_arg(ar, int));
+				break;
+			case 'f':
+				printf("%s%f", separ, va_arg(ar, double));
+				break;
+			case 's':
+				string = va_arg(ar, char *);
+				if (!string)
+					string = "(nil)";
+				printf("%s%s", separ, string);
+				break;
+			default:
+				i++;
+				continue;
 		}
+		separ = ", ";
+		i++;
 	}
 
-	printf("\n");
-
 	va_end(ar);
+	printf("\n");
 }
