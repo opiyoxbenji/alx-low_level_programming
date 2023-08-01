e98de4948dd92943227b1117bb4004c24c97c361
opiyoxbenji <opiyosbenji@gmail.com>
2023-08-01 19:35:35 +0300

    some fixes

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,8 +16,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 int _putchar(char c);
@@ -36,6 +36,6 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-const listint_t **_r(const listint_t **list, size_t size, const listint_t *new);
+
 
 #endif
