d1bee62fb7ff8cedfbdd2e2c7aa1e4ff5f26d25f
opiyoxbenji <opiyosbenji@gmail.com>
2023-08-07 13:15:08 +0300

    function that reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <fcntl.h>
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX std output
+ * @letters: the number of letters it should read and print
+ * @filename: pointer to the textfile
+ * Return: 0 if the file cannot be opened or read
+ *	if filename is NULL return 0
+ *	if write fails or doesn't worite the expected amount of bytes
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	size_t fd, num_read, num_written;
+	char *buff;
+	
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if ((ssize_t) fd == -1)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+	{
+		return (0);
+	}
+
+	num_read = read(fd, buff, letters);
+	if ((ssize_t) num_read == -1)
+	{
+		free(buff);
+		close(fd);
+		return (0);
+	}
+
+	num_written = write(STDOUT_FILENO, buff, num_read);
+	if (num_written != num_read)
+	{
+		free(buff);
+		close(fd);
+		return (0);
+	}
+
+	free(buff);
+	close(fd);
+	return (num_written);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,11 @@
 #ifndef main_h
 #define main_h
 
+#include <unistd.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
