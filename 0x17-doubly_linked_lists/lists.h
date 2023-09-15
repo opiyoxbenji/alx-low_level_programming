f0a36df9755f2bc851bbd8343126b37f265d7542
pixelBushi <opiyosbenji@gmail.com>
2023-09-15 05:03:06 +0300

    a function that prints all the elements of a dlistint_t list

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef linked_h
-#define linked_h
+#ifndef lists_h
+#define lists_h
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -8,6 +8,22 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ *
+ */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
