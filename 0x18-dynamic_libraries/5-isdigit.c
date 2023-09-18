33ac5c930021cb1e86d8c80062eacf45c72dc666
pixelBushi <opiyosbenji@gmail.com>
2023-09-18 19:17:11 +0300

    some fixes

--- 0x18-dynamic_libraries/5-isdigit.c
+++ 0x18-dynamic_libraries/5-isdigit.c
@@ -1,18 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char c;
+*_isupper - checks for uppercase
+*@c: characters to be checked
+*Return: 0 if lowercase, 1 if uppercase
+*/
 
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
