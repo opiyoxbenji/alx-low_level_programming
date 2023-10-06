9237115c73a417fde54b0a5deca466f3c997d50d
opiyoxbenji <opiyosbenji@gmail.com>
2023-10-06 03:47:46 +0300

    implements the djb2 algorithm

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,15 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <hash_tables.h>
+#include "hash_tables.h"
 
-/*
- * hash_djb2 - Calculate the DJB2 hash value for the given string.
- *
- * This function takes an input string and calculates its DJB2 hash value using
- * the DJB2 algorithm. The hash value is returned as an unsigned long int.
- *
- * @param str: The input string for which the hash is calculated.
- * @return: The DJB2 hash value of the input string.
+/**
+ * hash_djb2 - Hash function implementing the djb2 algorithm.
+ * @str: The string to hash.
+ * Return: The calculated hash.
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
@@ -18,9 +12,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 
 	hash = 5381;
 	while ((c = *str++))
-	{
-		hash = ((hash << 5) + hash) + c;
-	}
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
 	return (hash);
 }
