572d206548d16e1b191a3deaed9ee8d4aa66e469
opiyoxbenji <opiyosbenji@gmail.com>
2024-01-21 01:24:13 +0000

    changes

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,100 +1,64 @@
+#include <stdio.h>
 #include "search_algos.h"
 
 /**
- * recursive_search - searches for a value in an array of
- * integers using the Binary search algorithm
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
+ * binnary_search - searches for a value in a sorted array
+ * @array: pointer to the first element
+ * @low: starting point of index
+ * @high: ending point of index
+ * @value: value to search for
+ * Return: index where value is located
  */
-int recursive_search(int *array, size_t size, int value)
+int binnary_search(int *array, size_t low, size_t high, int value)
 {
-	size_t half = size / 2;
-	size_t i;
+	size_t mid, a;
 
-	if (array == NULL || size == 0)
-		return (-1);
-
-	printf("Searching in array");
-
-	for (i = 0; i < size; i++)
-		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
-
-	printf("\n");
-
-	if (half && size % 2 == 0)
-		half--;
-
-	if (value == array[half])
-		return ((int)half);
-
-	if (value < array[half])
-		return (recursive_search(array, half, value));
-
-	half++;
-
-	return (recursive_search(array + half, size - half, value) + half);
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+		printf("Searching in array: %d", array[low]);
+		for (a = low + 1; a <= high; a++)
+		{
+			printf(", %d", array[a]);
+		}
+		printf("\n");
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		else if (array[mid] < value)
+		{
+			low = mid + 1;
+		}
+		else
+		{
+			high = mid - 1;
+		}
+	}
+	return (-1);
 }
-
 /**
- * binary_search - calls to binary_search to return
- * the index of the number
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
- */
-int binary_search(int *array, size_t size, int value)
-{
-	int index;
-
-	index = recursive_search(array, size, value);
-
-	if (index >= 0 && array[index] != value)
-		return (-1);
-
-	return (index);
-}
-
-/**
- * exponential_search - searches for a value in an array of
- * integers using the Exponential search algorithm
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
+ * exponential_search - searches for a value in a sorted array
+ * @array: pointer to the first element
+ * @size: number of elements
+ * @value: value to search for
+ * Return: return the first index
  */
 int exponential_search(int *array, size_t size, int value)
 {
-	size_t index, next;
-	int result;
+	size_t bound = 1, low, high;
 
-	if (array == NULL)
-		return (-1);
-
-	if (array[0] == value)
-		return (0);
-
-	index = 1;
-
-	while (array[index] < value && index < size)
+	if (array == NULL || size == 0)
 	{
-		printf("Value checked array[%d] = [%d]\n", (int)index, array[index]);
-		index *= 2;
+		return (-1);
 	}
-
-	next = (index >= size) ? (size - 1) : index;
-
-	index /= 2;
-
-	printf("Value found between indexes [%d] and [%d]\n", (int)index, (int)next);
-
-	result = binary_search(array + index, (next + 1) - index, value);
-
-	if (result >= 0)
-		result += index;
-
-	return (result);
+	while (bound < size && array[bound] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
+		bound *= 2;
+	}
+	low = bound / 2;
+	high = (bound < size - 1) ? bound : size - 1;
+	printf("Value found between indexes [%lu] and [%lu]\n", low, high);
+	return (binnary_search(array, low, high, value));
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -48,8 +48,7 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
-int recursive_search(int *array, size_t size, int value);
-
+int binnary_search(int *array, size_t low, size_t high, int value);
 
 
 #endif
